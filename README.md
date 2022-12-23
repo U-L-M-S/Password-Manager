d63ec9b766f263d12c3db752415c6175fe966173
U-L-M-S <uinclis@outlook.de>
2022-12-23 23:49:33 +0100

    Create Password functions is done. NFF: add some errors messagers in case that the user use wrong syntax or comands

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Password-Manager
 It's a simple but powerfull password managiment system. Create Password and/or check if the user password has been leaked (HIBP)
+
+To use the code you will need to install some modules:
+```term
+npm install crypto https crypto-js
+```
+
+After that you are good to go.
+
+```term
+npm pms -h 
+```
+
+Try this command to check the options.
--- pms.js
+++ pms.js
@@ -1,38 +1,54 @@
 const https = require('https') // To make the resquest to HIBP
 const CryptoJS = require('crypto-js') // We need the crypto-js library to compute the SHA-1 hash
-
-// module.exports = async handleInput;
+const crypto = require('crypto') // We need it to create a SAFE random number
 
 // Get the second element in the process.argv array (the first element is the path to Node.js, and the second element is the path to the script)
 const input = process.argv[2]
-const user_password = process.argv[3]
+const input2 = process.argv[3]
+const input3 = process.argv[4]
 
 async function handleInput(input) {
   if (input === '-h' || input === '--help') {
-    showHelp();
+    showHelp()
   } else if (input === '-v' || input === '--version') {
-    showVersion();
-  } else if (input == '-hibp') {
-    if (
-      typeof user_password !== 'undefined' &&
-      typeof user_password !== 'null'
-    ) {
-      const result = await hibp(user_password);
-      console.log(result);
+    showVersion()
+  } else if (input === '-hibp' || input === '--have-i-been-pwned') {
+    if (input2 !== undefined && input2 !== null) {
+      hibp(input2)
+    }
+  } else if (input === '-cp' || input === '--create-password') {
+    if (validateInput(input2) === true) {
+      const result_cp = createPassword(input2)
+      console.log(result_cp)
     }
   } else {
-    console.log('Invalid input. Use -h or --help for a list of options.');
+    console.log('Invalid input. Use -h or --help for a list of options.')
   }
 }
 
+// Check if the user insected any character other than 'b', 's', 'c' (digits will be ignored)
+function validateInput(input) {
+  // Check if input contains any character other than 'b', 's', or 'c'
+  let invalidChars = input.match(/[^bscd\d]/g)
 
+  // If invalidChars is not null, return false
+  if (invalidChars) {
+    return false
+  }
+
+  // Otherwise, return true
+  return true
+}
 
 function showHelp() {
   console.log('Available options:')
   console.log('-h, --help: Show this help message')
   console.log('-v, --version: Show version information')
   console.log(
-    '-hibp: --check if your password has been leaked or not\n \t Example: node pms.js 123abc'
+    '-hibp, --have-i-been-pwned: Check if your password has been leaked or not\n \t EX: node pms.js 123abc'
+  )
+  console.log(
+    '-cp, --create-password: Create a randon password. Syntax:\n \t \t <number> length\n \t \t b big letters\n \t \t s small letters\n \t \t c characters\n \t d digits \n \t EX: -cp 12bc -> it creates a password with a length of 12 + big letters + characters'
   )
 }
 
@@ -42,42 +58,83 @@ function showVersion() {
 
 // Get the user input and turn it to sha1 (hash) and send to HIBP and get the output. Show user the output
 function hibp(password) {
-  return new Promise((resolve, reject) => {
-    // We return a Promise to handle the async nature of the API request
-    const hash = CryptoJS.SHA1(password).toString() // We hash the password using the SHA-1 algorithm
-    const options = {
-      hostname: 'api.pwnedpasswords.com',
-      path: `/range/${hash.substring(0, 5)}`, // We only need the first 5 characters of the hash for the API request
-      headers: {
-        'User-Agent': 'MyApp/1.0.0', // We need to set a User-Agent header to make the request
-      },
-    }
+  let hashedPassword = crypto
+    .createHash('sha1')
+    .update(password)
+    .digest('hex')
+    .toUpperCase()
+  let prefix = hashedPassword.slice(0, 5)
+  let apiCall = `https://api.pwnedpasswords.com/range/${prefix}`
 
-    https
-      .get(options, (res) => {
-        let data = ''
-        res.on('data', (chunk) => {
-          data += chunk
-        }) // We append each chunk of data to a string
-        res.on('end', () => {
-          const lines = data.split('\n') // The API returns the hash suffixes and number of occurrences on separate lines
-          for (const line of lines) {
-            const [suffix, count] = line.split(':') // We split each line by the colon separator
-            if (hash.substring(5).toUpperCase() === suffix.toUpperCase()) {
-              // If the suffix matches the remaining characters of the hash, the password has been leaked
-              resolve(
-                'This password has been found ${count} times in data breaches.'
-              )
-              return
-            }
-          }
-          resolve('This password has not been found in any data breaches.') // If we didn't find a match, the password has not been leaked
-        })
-      })
-      .on('error', (e) => {
-        reject(e) // If there was an error making the request, we reject the Promise with the error
-      })
-  })
+  let hashes = ''
+  https
+    .get(apiCall, function (res) {
+      res.setEncoding('utf8')
+      res.on('data', (chunk) => (hashes += chunk))
+      res.on('end', onEnd)
+    })
+    .on('error', function (err) {
+      console.error(`Error: ${err}`)
+    })
+
+  function onEnd() {
+    let res = hashes.split('\r\n').map((h) => {
+      let sp = h.split(':')
+      return {
+        hash: prefix + sp[0],
+        count: parseInt(sp[1]),
+      }
+    })
+
+    let found = res.find((h) => h.hash === hashedPassword)
+    if (found) {
+      console.log(`Password is vulnarable \n ${found.count} matches found!`)
+    } else {
+      console.log('Password is safe\n NO matches found!')
+    }
+  }
+}
+
+function createPassword(input) {
+  let password = ''
+
+  // If input contains digits, use the digits as the password length. Otherwise use 8 as the password length
+  let digitsRegex = /\d+/ // I hate REGEX btw :)
+  let digitsMatch = input.match(digitsRegex)
+  let length = digitsMatch ? parseInt(digitsMatch[0]) : 8
+
+  // Create an array of possible characters for each style
+  let capitalLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let smallLetters = 'abcdefghijklmnopqrstuvwxyz'
+  let specialChars = '-+_,;~@#$%^&*([{}])'
+  let digits = '0123456789'
+
+  // Initialize an array to store the possible characters
+  let possibleChars = []
+
+  // Add possible characters to the array based on the specified styles
+  if (input.includes('b')) {
+    possibleChars.push(...capitalLetters)
+  }
+  if (input.includes('s')) {
+    possibleChars.push(...smallLetters)
+  }
+  if (input.includes('c')) {
+    possibleChars.push(...specialChars)
+  }
+  if (input.includes('d')) {
+    possibleChars.push(...digits)
+  }
+
+  // Add random characters to the password
+  for (let i = 0; i < length; i++) {
+    let randomIndex = crypto.randomBytes(1).readUInt8(0) % possibleChars.length
+    let randomChar = possibleChars[randomIndex]
+    password += randomChar
+  }
+
+  // Return the generated password
+  return password
 }
 
 // Call the handleInput function with the user input
